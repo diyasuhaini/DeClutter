6ae4a05d2145233531a260afa3dc260c70a8774f
Nazirul <20ftt1440@student.pb.edu.bn>
2022-07-30 11:19:18 +0800

    DeClutter app created

---
+++ src/README.md
@@ -0,0 +1,3 @@
+replace this src file to your laptop
+
+make new branch to commit changes
---
+++ src/app/index/index-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IndexPage } from './index.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: IndexPage
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class IndexPageRoutingModule {}
