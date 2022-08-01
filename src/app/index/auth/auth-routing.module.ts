66881abc1be3664fcd0ac6396f389985fb5a4632
Nazirul <20ftt1440@student.pb.edu.bn>
2022-08-01 22:24:22 +0800

    otp layout added

--- src/app/index/auth/auth-routing.module.ts
+++ src/app/index/auth/auth-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
   {
     path: 'signup',
     loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
+  },
+  {
+    path: 'verification',
+    loadChildren: () => import('./verification/verification.module').then( m => m.VerificationPageModule)
   }
 ];
 
