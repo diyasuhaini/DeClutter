dbb9608631559ff0dac57f4d2791818ab63f302b
Eiji <20ftt1464@student.pb.edu.bn>
2022-08-02 12:02:16 +0800

    New Password Page Added

--- src/app/index/auth/forgot-password/forgot-password-routing.module.ts
+++ src/app/index/auth/forgot-password/forgot-password-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
   {
     path: '',
     component: ForgotPasswordPage
+  },
+  {
+    path: 'new-password',
+    loadChildren: () => import('./new-password/new-password.module').then( m => m.NewPasswordPageModule)
   }
 ];
 
