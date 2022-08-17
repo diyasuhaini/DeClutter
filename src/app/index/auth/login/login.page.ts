77083a50a94bdc63c50f8239473b066e7b824254
Nazirul <20ftt1440@student.pb.edu.bn>
2022-08-17 19:20:50 +0800

    homepage add layout

--- src/app/index/auth/login/login.page.ts
+++ src/app/index/auth/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  userForm: FormGroup;
+  
+
+  constructor(private router: Router,
+              private authenticationService: AuthenticationService,
+              private builder: FormBuilder) { }
 
   ngOnInit() {
+    this.userForm = this.builder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(8),
+        Validators.required
+      ])),
+    });
   }
 
 }
