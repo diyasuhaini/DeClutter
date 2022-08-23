5267ca6153b8cf585f9bdba4daebc9236ff95d1d
Nazirul <20ftt1440@student.pb.edu.bn>
2022-08-23 08:17:03 +0800

    retrieve database

--- src/app/index/mepage/mepage.page.ts
+++ src/app/index/mepage/mepage.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/authentication.service';
+import { Subscription } from 'rxjs';
+import { User } from '../auth/auth.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mepage',
@@ -8,9 +11,22 @@ import { AuthenticationService } from 'src/app/authentication.service';
 })
 export class MepagePage implements OnInit {
 
-  constructor(private authenticationService: AuthenticationService) { }
+  private people: User[];
+  private userSub: Subscription;
+
+  constructor(private router: Router,
+              private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
+    this.userSub = this.authenticationService.$users.subscribe(users => {
+      this.people = users;
+      console.log(this.people[1])
+    });
+  }
+
+  //get the item from database
+  ionViewWillEnter(){
+    this.authenticationService.fetchUser().subscribe();
   }
 
 }
