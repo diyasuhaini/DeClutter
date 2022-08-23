4c20e5abd276496958b05b34b07873f54cbeb024
Nazirul <20ftt1440@student.pb.edu.bn>
2022-08-23 09:56:33 +0800

    color fixed

--- src/app/index/mepage/mepage.page.ts
+++ src/app/index/mepage/mepage.page.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/authentication.service';
 import { Subscription } from 'rxjs';
 import { User } from '../auth/auth.model';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mepage',
@@ -14,13 +13,11 @@ export class MepagePage implements OnInit {
   private people: User[];
   private userSub: Subscription;
 
-  constructor(private router: Router,
-              private authenticationService: AuthenticationService) { }
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
     this.userSub = this.authenticationService.$users.subscribe(users => {
       this.people = users;
-      console.log(this.people[1])
     });
   }
 
