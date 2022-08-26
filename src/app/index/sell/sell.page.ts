56a04d393467c19e87183e981eef2bc94f27696b
Alif Saifullah (laptop) <20FTT1464@student.pb.edu.bn>
2022-08-26 12:30:16 +0800

    wip sellpage form

--- src/app/index/sell/sell.page.ts
+++ src/app/index/sell/sell.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sell',
@@ -7,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellPage implements OnInit {
 
-  constructor() { }
+  sellForm: FormGroup;
+  errorMsg: String = '';
+
+  error_msg = {
+    'img1': [
+      { 
+        type: 'required', 
+        message: 'Item image is required' 
+      }
+    ],
+    'img2': [
+      { 
+        type: 'required', 
+        message: 'Item image is required' 
+      }
+    ],
+    'img3': [
+      { 
+        type: 'required', 
+        message: 'Item image is required' 
+      }
+    ],
+  }
+
+  constructor(private builder: FormBuilder) { }
 
   ngOnInit() {
+    this.sellForm = this.builder.group({
+      img1: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      img2: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      img3: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      title: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      description: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      brand: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      type: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+    })
   }
 
 }
