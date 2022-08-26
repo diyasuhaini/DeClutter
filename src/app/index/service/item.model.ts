679f27b2d3a74b6b1f65faeed9f2e97058f9be10
Eiji <20ftt1464@student.pb.edu.bn>
2022-08-26 15:22:17 +0800

    added item service & item model

---
+++ src/app/index/service/item.model.ts
@@ -0,0 +1,16 @@
+import { UrlTree } from "@angular/router";
+
+//data here
+export class Item{
+    constructor(
+        public itemid: string,
+        public vendor: string,
+        public img1: string,
+        public img2: string,
+        public img3: string,
+        public title: string,
+        public description: string,
+        public brand: string,
+        public type: string
+    ){}
+}
