90bc7679520ab2dafe353f9eeee58213290db5de
Nazirul <20ftt1440@student.pb.edu.bn>
2022-08-30 00:49:23 +0800

    update

--- src/app/index/service/item.model.ts
+++ src/app/index/service/item.model.ts
@@ -1,6 +1,6 @@
 import { UrlTree } from "@angular/router";
 
-//data here
+//data for post here
 export class Item{
     constructor(
         public itemid: string,
@@ -17,5 +17,18 @@ export class Item{
         public categories: string,
         public brand: string,
         public type: string
+    ){}
+}
+
+
+//data for buy here
+export class sold{
+    constructor(
+        public itemid: string,
+        public customer: string,
+        public username: string,
+        public brand: string,
+        public price: string,
+        public img1: string,
     ){}
 }
