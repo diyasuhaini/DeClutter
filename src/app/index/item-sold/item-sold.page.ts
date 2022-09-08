d0c5237bdfb7a9e6cf061870b7cd2ce4555750cc
Alif Saifullah (laptop) <20FTT1464@student.pb.edu.bn>
2022-09-08 13:58:38 +0800

    item-sold fix

--- src/app/index/item-sold/item-sold.page.ts
+++ src/app/index/item-sold/item-sold.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthenticationService } from 'src/app/authentication.service';
+import { User } from '../auth/auth.model';
+import { Item } from '../service/item.model';
+import { ItemService } from '../service/item.service';
 
 @Component({
   selector: 'app-item-sold',
@@ -7,9 +12,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemSoldPage implements OnInit {
 
-  constructor() { }
+  private userSub: Subscription;
+  private people: User[];
+  private currentid: string;
+  private item: Item[];
+  private number1: number = 0;
+  private totalcost: number = 0;
+  private shipping: number = 0;
+  private discount: number = 1;
+  private discountprice: number = 0;
+  private finaltotal: number = 0;
+
+  constructor(private itemService: ItemService, private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
+    this.userSub = this.authenticationService.$users.subscribe(users => {
+      this.people = users;
+    });
+
+     this.userSub = this.authenticationService.$users.subscribe(users => {
+      this.people = users;
+      this.people.forEach((user) => {
+        if (user.email.toLowerCase() == localStorage.getItem('currentemail').toLowerCase()){
+          this.currentid = user.id;
+        }
+      });
+    });
+  }
+
+
+  ionViewWillEnter(){
+    this.authenticationService.fetchUser().subscribe();
+    this.itemService.retrieveCart().then((cart) => {
+      this.item = cart;
+      this.number1 = 0;
+      this.totalcost = 0;
+      this.finaltotal = 0;
+      this.item.forEach((item) => {
+        this.number1 += 1;
+        this.totalcost += parseInt(item.price);
+      });
+      this.finaltotal = (this.totalcost + this.shipping)/ this.discount;
+    })
   }
 
 }
