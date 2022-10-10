516e94ec849a0761f161a4519a599474e2785f18
Eiji <20ftt1464@student.pb.edu.bn>
2022-10-10 08:18:03 +0800

    messaging database working message

--- src/app/index/message/message.page.ts
+++ src/app/index/message/message.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { User } from '../auth/auth.model';
+import { MessageService } from '../service/message.service';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-message',
@@ -9,24 +10,29 @@ import { User } from '../auth/auth.model';
 })
 export class MessagePage implements OnInit {
 
-  private user = [];
+  private vendorid;
+  private vendorname;
+  private message: FormGroup;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private messageService: MessageService, private builder: FormBuilder) { }
 
   ngOnInit() {
+    this.message = new FormGroup({
+      text: new FormControl()
+    });
     // get current user id
     const currentid = localStorage.getItem('currentid');
     // get the router state from the previous page
     const routerState = this.router.getCurrentNavigation().extras.state;
     // turn the router state into an array
-    this.user = [{
-        vendor: routerState[0].vendor,
-        username: routerState[0].username
-    }];
-
-    
-
-
+      this.vendorid = routerState[0].vendor;
+      this.vendorname = routerState[0].username;
   }
+
+  sendmessage(text){
+    this.messageService.sendmessage(this.message.value.text, this.vendorid);
+  }
+
+
 
 }
--- src/app/index/service/message.service.ts
+++ src/app/index/service/message.service.ts
@@ -1,9 +1,55 @@
 import { Injectable } from '@angular/core';
+import { get, getDatabase, ref, remove, set, update } from "firebase/database";
+import { environment } from 'src/environments/environment';
+import { initializeApp } from 'firebase/app';
+import { Console } from 'console';
+
+// initialize the application allow new database apit to be used
+initializeApp(environment.firebaseConfig);
+// connection to realtime
+const database = getDatabase();
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
+  
+  private currentid = localStorage.getItem('currentid');
+
 
   constructor() { }
+
+  async sendmessage(text, nduser){
+    const currentid = this.currentid;
+    const comb1 = currentid + nduser;
+    const comb2 = nduser + currentid;
+    var finaldist = comb1;
+    const dbref = ref(database, 'message/');
+    const snapshot = await get((dbref));
+    const check = snapshot.val();
+    var checker = [];
+    console.log(check);
+    if(!check){
+      return set(ref(database, 'message/' + finaldist), [{text: text, user: currentid}]);
+    } else {
+      Object.keys(check).forEach(key => {
+        if (key == comb2){
+          finaldist = comb2;
+        }
+      });
+      // check.push({text: text, user: currentid});
+      console.log(finaldist);
+      const dbref2 = ref(database, 'message/' + finaldist);
+      const snapshot = await get((dbref2));
+      const check2 = snapshot.val();
+      Object.keys(check2).forEach(key => {
+        checker.push(check2[key]);
+      })
+      checker.push({text: text, user: currentid});
+      console.log("checker", checker);
+      console.log("finaldist", finaldist);
+      return set(ref(database, 'message/' + finaldist), checker);
+    }
+  }
+
 }
