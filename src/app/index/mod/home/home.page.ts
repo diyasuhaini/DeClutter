ae87e00a42338506dce299a9f93dba2c358f1ab0
Nazirul <20ftt1440@student.pb.edu.bn>
2022-10-18 22:45:42 +0800

    update user for moderator

--- src/app/index/mod/home/home.page.ts
+++ src/app/index/mod/home/home.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, Subscription } from 'rxjs';
 import { report } from '../../service/item.model';
 import { ReportService } from '../../service/report.service';
+import { AuthenticationService } from 'src/app/authentication.service';
+import { User } from '../../auth/auth.model';
 
 @Component({
   selector: 'app-home',
@@ -11,8 +13,11 @@ import { ReportService } from '../../service/report.service';
 export class HomePage implements OnInit {
 
   private reports: report[];
+  private people: User[]; //for moderator user
+  private userSub: Subscription;
 
-  constructor(private reportService: ReportService) { }
+  constructor(private reportService: ReportService,
+              private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
     this.reportService.getReport().then((list) => {
@@ -20,5 +25,28 @@ export class HomePage implements OnInit {
       this.reports = list.reverse();
       console.log(this.reports);
     })
+
+
+    //for retrieve user data
+    this.userSub = this.authenticationService.$users.subscribe(users => {
+      this.people = users;
+    });
+
+    this.userSub = this.authenticationService.$users.subscribe(users => {
+      console.log(users);
+      this.people.forEach((user) => {
+        if(user.email.toLowerCase() == localStorage.getItem('currentemail').toLowerCase()){ //if similar to current email
+          localStorage.setItem('currentid', user.id); //send new currentid to localstorage
+        }else{
+          console.log("not good"); //if error
+        }
+      })
+    })
+
+    console.log(this.userSub);
+  }
+
+  ionViewWillEnter(){
+    this.authenticationService.fetchUser().subscribe();
   }
 }
