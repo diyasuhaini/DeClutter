8acad1d4d25604da71fdf67f0f056770de4ace9a
Nazirul <20ftt1440@student.pb.edu.bn>
2022-10-27 22:21:45 +0800

    fix reports

--- src/app/index/inbox/inbox.page.ts
+++ src/app/index/inbox/inbox.page.ts
@@ -15,17 +15,17 @@ export class InboxPage implements OnInit {
   constructor(private replyService: ReplyEmailService) { }
 
   ngOnInit() {
-    var cuid = localStorage.getItem('currentname');
-    this.replyService.getReply().then((theReply) => {
-      console.log(theReply);
-      theReply.forEach((items) => {
-        if(cuid == items.username){
-          this.box.push(items);
+    var cuid = localStorage.getItem('currentname'); //retrieve from localstorage
+    this.replyService.getReply().then((theReply) => { //get the service
+      console.log(theReply); //check if the value is correct
+      theReply.forEach((items) => { //separate the item
+        if(cuid == items.username){ //check if the item is the real owner
+          this.box.push(items); //if correct then push it the empty box
         }
-        console.log(items);
+        console.log(items); //check every item
       })
     })
-    this.replies = this.box.reverse();
+    this.replies = this.box.reverse(); //in reverse
   }
 
   ionViewWillEnter(){
